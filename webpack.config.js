46ae0cda1430e42894f142f2610d7fe0351c3ead
Dmitrii Bulatov <bulati484@gmail.com>
2024-02-19 13:57:17 +0300

    correct webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'product'),
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
